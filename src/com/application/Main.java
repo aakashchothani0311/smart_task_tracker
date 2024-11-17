8d978d0b4688105d384a3ee546cc2ceccf8c1249
Mann Bankimbhai Patel <patel.mannb@northeastern.edu>
2024-11-17 17:25:56 -0500

    Initial Setup

--- src/com/application/Main.java
+++ src/com/application/Main.java
@@ -18,28 +18,27 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws IOException {
 		stg = primaryStage;
 		
-		Parent root = FXMLLoader.load(getClass().getResource(viewPath + "Login.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource(viewPath + "Main.fxml"));
 		Scene scene = new Scene(root);
 				
-		primaryStage.setTitle("SMART Task Tracker");
+		primaryStage.setTitle("SMART Task Tracker"); 
 		primaryStage.setScene(scene);
+		primaryStage.setFullScreen(true);
 		primaryStage.show();
 	}
 	
 	public void successfulLogin() throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource(viewPath + "Main.fxml"));
-		Scene scene = new Scene(root, 1000, 1000);
+		Scene scene = new Scene(root);
+		
+		//stg.setMaximized(true);
 		
 		stg.setTitle("My Tasks");
 		stg.setScene(scene);
-				
-		stg.setOnCloseRequest(event -> {
-		//	dbc.disconnectDB();			
-		});
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
-		//TasksFileUtil.readAllTasks();
+//		TasksFileUtil.readAllTasks();
 	}
 }
