eb1d078df096b4d1f46d95d851aab976f17af6fd
Aakash Chothani <chothani.aa@northeastern.edu>
2024-11-30 16:24:19 -0500

    Main.FXMl changes

--- src/com/application/util/CustomHeapInterface.java
+++ src/com/application/util/CustomHeapInterface.java
@@ -1,7 +1,10 @@
 package com.application.util;
 
+import java.util.List;
+
 public interface CustomHeapInterface<T> {
 	public void insert(T val);
 	public T remove();
+	public List<T> toList();
 	public boolean isEmpty(); 
 }
