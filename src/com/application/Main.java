f3c594b9fc713eab6b07cf0b6428dd006ea52527
Aakash Chothani <chothani.aa@northeastern.edu>
2024-12-01 16:57:21 -0500

    UI completed

--- src/com/application/Main.java
+++ src/com/application/Main.java
@@ -20,36 +20,35 @@ public class Main extends Application {
 		stg = primaryStage;
 		
 		Parent root = FXMLLoader.load(getClass().getResource(viewPath + "Main.fxml"));
-		Scene scene = new Scene(root);
 				
-		primaryStage.setTitle("SMART Task Tracker"); 
-		primaryStage.setScene(scene);
+		primaryStage.setTitle("Login"); 
+		primaryStage.setScene(new Scene(root));
 		
 		primaryStage.setMinWidth(1200);
 	    primaryStage.setMinHeight(800);
 		primaryStage.show();
 	}
 	
-	/*public void successfulLogin() throws IOException {
+	public void successfulLogin() throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource(viewPath + "Main.fxml"));
 		Scene scene = new Scene(root);
 		
-		//stg.setMaximized(true);
-		
-		stg.setTitle("My Tasks");
+		stg.setTitle("SMART Task Tracker");
 		stg.setScene(scene);
-	}*/
+	}
 	
 	public static void main(String[] args) {
-		launch(args);	
+		//launch(args);	
 		
-		/*CustomHeap<Integer> heap = new CustomHeap<Integer>();
+		CustomHeap<Integer> heap = new CustomHeap<Integer>();
 		int[] temp = new int[] {5, 7, 8, 1, 2,5, 10, -2};
+		
+//		int[] temp = new int[] {7, 5, 2, 1};
 		
 		for(int t : temp)
 			heap.insert(t);
 		
 		for(int t : heap.toList())
-			System.out.println(t);*/
+			System.out.println(t);
 	}
 }
